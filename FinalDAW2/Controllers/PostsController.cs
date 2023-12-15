c703c372a9e9cad6a53d8d402e4b7c3a43e2daa4
Popa Andrei <andreipopa.ro@gmail.com>
2023-12-15 18:38:53 +0200

    da

--- FinalDAW2/Controllers/PostsController.cs
+++ FinalDAW2/Controllers/PostsController.cs
@@ -2,6 +2,7 @@
 using FinalDAW2.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ProiectDAW.Models;
 
 namespace ProiectDAW.Controllers
@@ -26,6 +27,76 @@ namespace ProiectDAW.Controllers
             _roleManager = roleManager;
         }
         public IActionResult Index()
+        {
+            var posts = db.Posts;
+            ViewBag.Posts = posts;
+
+            if (TempData.ContainsKey("message"))
+            {
+                ViewBag.Message = TempData["message"];
+            }
+
+            return View();
+        }
+        public IActionResult New()
+        {
+            Post article = new Post();
+            return View(article);
+        }
+
+
+        [HttpPost]
+        public IActionResult New(Post post)
+        {
+            post.DataPostarii = DateTime.Now;
+            if (ModelState.IsValid)
+            {
+                db.Posts.Add(post);
+                db.SaveChanges();
+                TempData["message"] = "Articolul a fost adaugat";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(post);
+            }
+        }
+        
+        
+
+
+        // Se adauga articolul modificat in baza de date
+        [HttpPost]
+        public IActionResult Edit(int id, Post requestPost)
+        {
+            Post post = db.Posts.Find(id);
+            
+            if (ModelState.IsValid)
+            {
+                post.Continut= requestPost.Continut;
+                db.SaveChanges();
+                TempData["message"] = "Articolul a fost modificat";
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                return View(requestPost);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {   ///identificare articol + stergere + salvare shimbari + mesaj
+            Post post= db.Posts.Find(id);
+            db.Posts.Remove(post);
+            db.SaveChanges();
+            TempData["message"] = "Articolul a fost sters";
+            return RedirectToAction("Index");
+        }
+
+       
+        public IActionResult IndexNou()
         {
             return View();
         }
--- FinalDAW2/Models/Post.cs
+++ FinalDAW2/Models/Post.cs
@@ -18,6 +18,7 @@ namespace ProiectDAW.Models
         // Alte atribute relevante
         public int? NumarLikeuri { get; set; }
         public string? UserId { get; set; }
+
         public virtual ApplicationUser? User { get; set; }
         public virtual ICollection<Comment>? Comments { get; set; }
 
